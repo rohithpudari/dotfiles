4ef7a29cdca1b9551d3b03e096f3fe8301fe3709
Rohith Pudari <27728974+rohithpudari@users.noreply.github.com>
2025-04-15 17:25:57 -0400

    alias ff and formatter

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -27,6 +27,9 @@ alias rm='rm -i'
 # Open a Finder window in your current directory
 alias f='open -a Finder ./'
 
+# open fzf tmux window for file search
+alias ff = 'fzf --tmux'
+
 # search for a file using fuzzy finder and open in neovim
 alias fo="fd --type f | fzf | sed 's/\ /\\\ /g' | xargs nvim"
 
@@ -35,22 +38,22 @@ alias j=z
 
 # Update dotfiles
 dfu() {
-  (
-    cd ~/.dotfiles && git pull --ff-only && ./install -q
-  )
+    (
+        cd ~/.dotfiles && git pull --ff-only && ./install -q
+    )
 }
 
 # Use pip without requiring virtualenv
 syspip() {
-  PIP_REQUIRE_VIRTUALENV="" pip "$@"
+    PIP_REQUIRE_VIRTUALENV="" pip "$@"
 }
 
 syspip2() {
-  PIP_REQUIRE_VIRTUALENV="" pip2 "$@"
+    PIP_REQUIRE_VIRTUALENV="" pip2 "$@"
 }
 
 syspip3() {
-  PIP_REQUIRE_VIRTUALENV="" pip3 "$@"
+    PIP_REQUIRE_VIRTUALENV="" pip3 "$@"
 }
 
 # cd to git root directory
@@ -58,51 +61,51 @@ alias cdgr='cd "$(git root)"'
 
 # Create a directory and cd into it
 mcd() {
-  mkdir "${1}" && cd "${1}"
+    mkdir "${1}" && cd "${1}"
 }
 
 # Jump to directory containing file
 jump() {
-  cd "$(dirname ${1})"
+    cd "$(dirname ${1})"
 }
 
 # cd replacement for screen to track cwd (like tmux)
 scr_cd() {
-  builtin cd $1
-  screen -X chdir "$PWD"
+    builtin cd $1
+    screen -X chdir "$PWD"
 }
 
 if [[ -n $STY ]]; then
-  alias cd=scr_cd
+    alias cd=scr_cd
 fi
 
 # Go up [n] directories
 up() {
-  local cdir="$(pwd)"
-  if [[ "${1}" == "" ]]; then
-    cdir="$(dirname "${cdir}")"
-  elif ! [[ "${1}" =~ ^[0-9]+$ ]]; then
-    echo "Error: argument must be a number"
-  elif ! [[ "${1}" -gt "0" ]]; then
-    echo "Error: argument must be positive"
-  else
-    for ((i = 0; i < ${1}; i++)); do
-      local ncdir="$(dirname "${cdir}")"
-      if [[ "${cdir}" == "${ncdir}" ]]; then
-        break
-      else
-        cdir="${ncdir}"
-      fi
-    done
-  fi
-  cd "${cdir}"
+    local cdir="$(pwd)"
+    if [[ "${1}" == "" ]]; then
+        cdir="$(dirname "${cdir}")"
+    elif ! [[ "${1}" =~ ^[0-9]+$ ]]; then
+        echo "Error: argument must be a number"
+    elif ! [[ "${1}" -gt "0" ]]; then
+        echo "Error: argument must be positive"
+    else
+        for ((i = 0; i < ${1}; i++)); do
+            local ncdir="$(dirname "${cdir}")"
+            if [[ "${cdir}" == "${ncdir}" ]]; then
+                break
+            else
+                cdir="${ncdir}"
+            fi
+        done
+    fi
+    cd "${cdir}"
 }
 
 # Execute a command in a specific directory
 xin() {
-  (
-    cd "${1}" && shift && "${@}"
-  )
+    (
+        cd "${1}" && shift && "${@}"
+    )
 }
 
 # Check if a file contains non-ascii characters
@@ -113,31 +116,31 @@ xin() {
 # Fetch pull request
 
 fpr() {
-  if ! git rev-parse --git-dir >/dev/null 2>&1; then
-    echo "error: fpr must be executed from within a git repository"
-    return 1
-  fi
-  (
-    cdgr
-    if [ "$#" -eq 1 ]; then
-      local repo="${PWD##*/}"
-      local user="${1%%:*}"
-      local branch="${1#*:}"
-    elif [ "$#" -eq 2 ]; then
-      local repo="${PWD##*/}"
-      local user="${1}"
-      local branch="${2}"
-    elif [ "$#" -eq 3 ]; then
-      local repo="${1}"
-      local user="${2}"
-      local branch="${3}"
-    else
-      echo "Usage: fpr [repo] username branch"
-      return 1
+    if ! git rev-parse --git-dir >/dev/null 2>&1; then
+        echo "error: fpr must be executed from within a git repository"
+        return 1
     fi
+    (
+        cdgr
+        if [ "$#" -eq 1 ]; then
+            local repo="${PWD##*/}"
+            local user="${1%%:*}"
+            local branch="${1#*:}"
+        elif [ "$#" -eq 2 ]; then
+            local repo="${PWD##*/}"
+            local user="${1}"
+            local branch="${2}"
+        elif [ "$#" -eq 3 ]; then
+            local repo="${1}"
+            local user="${2}"
+            local branch="${3}"
+        else
+            echo "Usage: fpr [repo] username branch"
+            return 1
+        fi
 
-    git fetch "git@github.com:${user}/${repo}" "${branch}:${user}/${branch}"
-  )
+        git fetch "git@github.com:${user}/${repo}" "${branch}:${user}/${branch}"
+    )
 }
 
 # Mirror a website
